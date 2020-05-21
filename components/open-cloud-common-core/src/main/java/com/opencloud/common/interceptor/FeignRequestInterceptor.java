a6c411c9db94bed95431f1966bff458c9d8c7d01
soler <515608851@qq.com>
2020-05-21 16:02:23 +0800

    修改fegin转发 请求头重复添加,导致调用失败的问题

--- components/open-cloud-common-core/src/main/java/com/opencloud/common/interceptor/FeignRequestInterceptor.java
+++ components/open-cloud-common-core/src/main/java/com/opencloud/common/interceptor/FeignRequestInterceptor.java
@@ -23,21 +23,27 @@ public class FeignRequestInterceptor implements RequestInterceptor {
 
     @Override
     public void apply(RequestTemplate template) {
-        HttpServletRequest httpServletRequest =   getHttpServletRequest();
-        if(httpServletRequest!=null){
+        HttpServletRequest httpServletRequest = getHttpServletRequest();
+        if (httpServletRequest != null) {
             Map<String, String> headers = getHeaders(httpServletRequest);
             // 传递所有请求头,防止部分丢失
             Iterator<Map.Entry<String, String>> iterator = headers.entrySet().iterator();
             while (iterator.hasNext()) {
                 Map.Entry<String, String> entry = iterator.next();
-                template.header(entry.getKey(), entry.getValue());
+                // 防止添加重复
+                if(!template.headers().containsKey(entry.getKey())){
+                    template.header(entry.getKey(), entry.getValue());
+                }
             }
-            // 微服务之间传递的唯一标识,区分大小写所以通过httpServletRequest获取
-            if (httpServletRequest.getHeader(X_REQUEST_ID)==null) {
-                String sid = String.valueOf(UUID.randomUUID());
-                template.header(X_REQUEST_ID, sid);
+            // 微服务之间传递的唯一标识,区分大小写所以通过httpServletRequest查询
+            if (headers.containsKey(X_REQUEST_ID)) {
+                String traceId = headers.get(X_REQUEST_ID);
+                MDC.put("traceId", traceId);
+                log.info("FeignRequestInterceptor:{}", traceId);
             }
-            log.debug("FeignRequestInterceptor:{}", template.toString());
+            if (log.isDebugEnabled()) {
+                log.debug("FeignRequestInterceptor:{}", template.toString());
+            }
         }
     }
 
