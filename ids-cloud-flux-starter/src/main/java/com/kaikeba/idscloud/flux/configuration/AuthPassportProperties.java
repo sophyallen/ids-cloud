7d1f8c0e76ebc354ef179423cf71540db4cfc5c3
mczhang <mczhang@kaikeba.com>
2022-06-03 14:41:10 +0800

    add multi domain auth

--- ids-cloud-flux-starter/src/main/java/com/kaikeba/idscloud/flux/configuration/AuthPassportProperties.java
+++ ids-cloud-flux-starter/src/main/java/com/kaikeba/idscloud/flux/configuration/AuthPassportProperties.java
@@ -1,7 +1,11 @@
 package com.kaikeba.idscloud.flux.configuration;
 
 import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import java.util.Map;
 
 /**
  * @author: zhangminchao
@@ -10,9 +14,39 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
  */
 @ConfigurationProperties(prefix = "auth.passport")
 @Data
+@Slf4j
 public class AuthPassportProperties {
+    @Deprecated
     private String group;
+    @Deprecated
     private String appId;
     private String baseUrl;
     private String userInfoUrl;
+    private String defaultGroup;
+    private Map<String, GroupProperties> groups;
+
+    @Data
+    public static class GroupProperties {
+        private String appId;
+        private String group;
+    }
+    public GroupProperties getGroupByHostName(String hostName) {
+        String groupName = getGroupNameFromHostName(hostName);
+        log.debug("get group name from hostname[{}] is : {}", hostName, groupName);
+        if (StringUtils.isBlank(groupName) || !groups.containsKey(groupName)) {
+            return groups.get(defaultGroup);
+        }
+        return groups.get(groupName);
+    }
+    public String getGroupNameFromHostName(String hostName) {
+        if (StringUtils.isBlank(hostName)) {
+            return null;
+        }
+        String[] splits = hostName.split("\\.");
+        if (splits.length <= 2) {
+            return null;
+        }
+        String groupName = splits[splits.length-2];
+        return groupName;
+    }
 }
